33fbd9a614181312c0a025b355c370768d964f47
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-12 17:48:11 +0200

    Implement router setup with AppLayout and DashboardView components

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardView from "./views/DashboardView";
 
 export default function Router() {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
-                    
+                <Route element={<AppLayout/>}>
+                    <Route path="/" element={<DashboardView/>} index/>
                 </Route>
             </Routes>
         </BrowserRouter>
