002037be4bba9b4a0e0f2fb0861bc694d504daf3
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-13 18:22:41 +0200

    Handle API errors in CreateProjectView; display error messages using toast notifications

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,11 +1,14 @@
 import type { ProjectFormData } from "@/types/index";
 import api from "@/lib/axios";
+import { isAxiosError } from "axios";
 
 export async function createProject(formaData : ProjectFormData) {
     try {
         const {data} = await api.post('/projects', formaData)
         return data       
     } catch (error) {
-        
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
     }
 }
