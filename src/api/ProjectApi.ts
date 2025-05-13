626e98f1d051548c94a26ae899b40166032e3eff
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-13 19:18:29 +0200

    Refactor DashboardView to use react-query for fetching projects; implement loading state and conditional rendering for project list

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,14 +1,28 @@
-import type { ProjectFormData } from "@/types/index";
+import { dashboardProjectSchema, type ProjectFormData } from "@/types/index";
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 
-export async function createProject(formaData : ProjectFormData) {
-    try {
-        const {data} = await api.post('/projects', formaData)
-        return data       
-    } catch (error) {
-        if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error)
-        }
+export async function createProject(formaData: ProjectFormData) {
+  try {
+    const { data } = await api.post("/projects", formaData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
     }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api.get("/projects");
+    const response = dashboardProjectSchema.safeParse(data);
+    if (response.success) {
+        return response.data
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
 }
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,20 +1,41 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjects } from "@/api/ProjectApi";
 
 export default function DashboardView() {
-  return (
-    <>
-      <h1 className="text-5xl font-black">Mis proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Maneja y administra tus proyectos
-      </p>
-      <nav className="my-5">
-        <Link
-          className="bg-purple-500 hover:bg-purple-600 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to="/projects/create"
-        >
-          Nuevo proyecto
-        </Link>
-      </nav>
-    </>
-  );
+  const { data, isLoading } = useQuery({
+    queryKey: ["projects"],
+    queryFn: getProjects,
+  });
+
+  if (isLoading) return "Cargando...";
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">Mis proyectos</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Maneja y administra tus proyectos
+        </p>
+        <nav className="my-5">
+          <Link
+            className="bg-purple-500 hover:bg-purple-600 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to="/projects/create"
+          >
+            Nuevo proyecto
+          </Link>
+        </nav>
+        {data.length ? (
+          data.map((project) => <p key={project._id}>{project.projectName}</p>)
+        ) : (
+          <p className="text-center py-20">
+            No hay proyectos aún <br/>
+            <Link
+              className="text-fuchsia-600 font-bold"
+              to="/projects/create"
+            >Crear proyecto</Link>
+          </p>
+        )}
+      </>
+    );
 }
