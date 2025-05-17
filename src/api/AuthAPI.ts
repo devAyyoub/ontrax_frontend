4153a9d6c6459f295d882b8515919116674c393a
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-17 21:54:25 +0200

    feat: implement account confirmation and request new code functionality

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,11 +1,35 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { UserRegistrationForm } from "../types";
+import type { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
     const url = '/auth/create-account'
     const {data} = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function confirmAccount(token: ConfirmToken) {
+  try {
+    const url = '/auth/confirm-account'
+    const {data} = await api.post<string>(url, token)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function requestConfirmationCode(email: RequestConfirmationCodeForm) {
+  try {
+    const url = '/auth/request-code'
+    const {data} = await api.post<string>(url, email)
     return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
