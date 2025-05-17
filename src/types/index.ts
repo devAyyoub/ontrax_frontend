7909cf34bd519dfbf81e8ba6800ec399ebe2cf2b
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-17 16:52:20 +0200

    Add authentication layout and login view; implement user login form with validation and error handling.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,16 @@
 import {z} from 'zod'
 
+/** Auth & Users */
+const authSchema = z.object({
+    name: z.string().email(),
+    email: z.string(),
+    password: z.string(),
+    password_confirmation: z.string()
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+
 /** Tasks */
 export const TaskStatusSchema = z.enum(["pending", "onHold", "inProgress", "under_review", "completed"])
 export type TaskStatus = z.infer<typeof TaskStatusSchema>
