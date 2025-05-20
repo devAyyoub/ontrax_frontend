e6dfe3798b76f280c236438422282c05dc5a57cd
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-20 21:43:48 +0200

    feat: implement logout functionality in NavMenu component

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import type { User } from '../types'
 
 type NavMenuProps = {
@@ -9,7 +9,11 @@ type NavMenuProps = {
 }
 
 export default function NavMenu({name} : NavMenuProps) {
-
+  const navigate = useNavigate()
+  const logout = () => {
+    localStorage.removeItem('AUTH_TOKEN')
+    navigate('/auth/login')
+  }
   return (
     <Popover className="relative">
       <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
@@ -39,7 +43,7 @@ export default function NavMenu({name} : NavMenuProps) {
             <button
               className='block p-2 hover:text-purple-950'
               type='button'
-              onClick={() => { }}
+              onClick={logout}
             >
               Cerrar Sesión
             </button>
