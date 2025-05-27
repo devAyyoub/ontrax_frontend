ca5aabdf605e2477f5730993d8cb493d71d77613
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-27 19:55:31 +0200

    feat: implement delete note functionality in NoteDetail component and update NoteApi for note deletion

--- src/api/NoteApi.ts
+++ src/api/NoteApi.ts
@@ -1,21 +1,42 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { NoteFormData, Project, Task } from "../types";
+import type { Note, NoteFormData, Project, Task } from "../types";
 
 type NoteApiType = {
-    formData: NoteFormData,
-    projectId: Project['_id']
-    taskId: Task['_id']
-}
+  formData: NoteFormData;
+  projectId: Project["_id"];
+  taskId: Task["_id"];
+  noteId: Note["_id"];
+};
 
-export const createNote = async ({projectId, taskId, formData} : Pick<NoteApiType, 'formData' | 'projectId' | 'taskId'>) => {
-    try {
-        const url = `/projects/${projectId}/tasks/${taskId}/notes`
-        const {data} = await api.post<string>(url, formData)
-        return data
-    } catch (error) {
+export const createNote = async ({
+  projectId,
+  taskId,
+  formData,
+}: Pick<NoteApiType, "formData" | "projectId" | "taskId">) => {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes`;
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
     }
   }
-}
+};
+
+export const deleteNote = async ({
+  projectId,
+  taskId,
+  noteId,
+}: Pick<NoteApiType, "projectId" | "taskId" | "noteId">) => {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`;
+    const { data } = await api.delete<string>(url);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,22 +1,66 @@
+import { deleteNote } from "@/api/NoteApi";
+import { useAuth } from "@/hooks/useAuth";
 import type { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type NoteDetailProps = {
   note: Note;
 };
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+  const { data, isLoading } = useAuth();
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+  const params = useParams();
+  const projectId = params.projectId!;
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("viewTask")!;
+  const queryclient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      queryclient.invalidateQueries({ queryKey: ["task", taskId] });
+      toast.success(data);
+    },
+  });
+
+  const handleDeleteNote = () => {
+    const data = {
+      projectId,
+      taskId,
+      noteId: note._id,
+    };
+    mutate(data);
+  };
+
+  if (isLoading) return "Cargando...";
   return (
-    <div className="flex justify-between items-center">
+    <div className="py-2 flex justify-between items-center">
       <div>
         <p>
-          <span className="font-bold ">{note.createdBy.name}</span>{" "}
+          <span className="font-bold text-fuchsia-600">
+            {note.createdBy.name}:
+          </span>{" "}
           {note.content}
         </p>
-        <p className="text-xs text-slate-500">
-          {formatDate(note.createdAt)}
-        </p>
+        <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+      {canDelete && (
+        <button
+          type="button"
+          onClick={handleDeleteNote}
+          className="bg-red-500 hover:bg-red-600 p-2 text-s text-white font-bold cursor-pointer transition-colors"
+        >
+          Eliminar
+        </button>
+      )}
     </div>
   );
 }
