2fedbba6546c87a603221d5e964b033e630382f2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-28 18:31:35 +0200

    feat: implement change password functionality with validation in ChangePasswordView

--- src/api/ProfileApi.ts
+++ src/api/ProfileApi.ts
@@ -1,10 +1,22 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { UserProfileForm } from "../types";
+import type { UpdateCurrentUserPasswordForm, UserProfileForm } from "../types";
 
 export const updateProfile = async (formData: UserProfileForm) => {
   try {
     const url = "/auth/profile";
+    const { data } = await api.put<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
+
+export const changePassword = async (formData: UpdateCurrentUserPasswordForm) => {
+  try {
+    const url = "/auth/update-password";
     const { data } = await api.put<string>(url, formData);
     return data;
   } catch (error) {
