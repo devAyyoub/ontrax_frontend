130cf9f3b4ac0a553cbe794a3b11e4bc55e7f431
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-28 20:23:51 +0200

    feat: integrate drag-and-drop functionality for task management using @dnd-kit/core

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,10 +1,16 @@
-import type { Task } from "@/types/index";
+import { DndContext, type DragEndEvent } from "@dnd-kit/core";
+import type { Task, TaskStatus } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { statusTranslations } from "@/locales/es";
+import DropTask from "./DropTask";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateStatus } from "@/api/TaskApi";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 type TaskListProps = {
   tasks: Task[];
-  canEdit: boolean
+  canEdit: boolean;
 };
 
 type GroupedTasks = {
@@ -28,34 +34,68 @@ const statusStyles: { [key: string]: string } = {
 };
 
 export default function TaskList({ tasks, canEdit }: TaskListProps) {
+  const params = useParams()
+  const projectId = params.projectId!
+  const queryclient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: updateStatus,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+      queryclient.invalidateQueries({ queryKey: ["project", projectId] });
+    },
+  });
+
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
     currentGroup = [...currentGroup, task];
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
+
+  const handleDragEnd = (e : DragEndEvent) => {
+    const { over, active } = e
+
+    if (over && over.id) {
+      const taskId = active.id.toString();
+      const status = over.id as TaskStatus
+
+      mutate({projectId, taskId, status})
+    } 
+    
+  }
+
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
 
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
-        {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
-            <h3
-              className={`text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]} text-center`}
-            >
-              {statusTranslations[status]}
-            </h3>
-            <ul className="mt-5 space-y-5">
-              {tasks.length === 0 ? (
-                <li className="text-gray-500 text-center pt-3">
-                  No Hay tareas
-                </li>
-              ) : (
-                tasks.map((task) => <TaskCard key={task._id} task={task} canEdit={canEdit}/>)
-              )}
-            </ul>
-          </div>
-        ))}
+        <DndContext onDragEnd={handleDragEnd}>
+          {Object.entries(groupedTasks).map(([status, tasks]) => (
+            <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+              <h3
+                className={`text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]} text-center`}
+              >
+                {statusTranslations[status]}
+              </h3>
+
+              <DropTask status={status}/>
+
+              <ul className="mt-5 space-y-5">
+                {tasks.length === 0 ? (
+                  <li className="text-gray-500 text-center pt-3">
+                    No Hay tareas
+                  </li>
+                ) : (
+                  tasks.map((task) => (
+                    <TaskCard key={task._id} task={task} canEdit={canEdit} />
+                  ))
+                )}
+              </ul>
+            </div>
+          ))}
+        </DndContext>
       </div>
     </>
   );
