32c9f1abcb8b22c084d160b5931cd0898e997162
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-28 19:08:09 +0200

    feat: add check password functionality and delete project modal component

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -2,6 +2,7 @@ import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 import {
   userSchema,
+  type CheckPasswordForm,
   type ConfirmToken,
   type ForgotPasswordForm,
   type NewPasswordForm,
@@ -105,11 +106,24 @@ export async function updatePasswordWithToken({
 
 export async function getUser() {
   try {
-    const { data } = await api.get("/auth/user");
+    const url = "/auth/user"
+    const { data } = await api.get(url);
     const response = userSchema.safeParse(data)
     if(response.success) {
       return response.data
     }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function checkPassword(formData: CheckPasswordForm) {
+  try {
+    const url = "/auth/check-password"
+    const { data } = await api.post<string>(url, formData);
+    return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
