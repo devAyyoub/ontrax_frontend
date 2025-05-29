b777470bb220c39763aa07d78f5e30a7b594b8ed
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-29 21:39:33 +0200

    fix: refactor logout function to accept close callback and improve component structure

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -13,16 +13,18 @@ export default function NavMenu({ name }: NavMenuProps) {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
 
-  const logout = () => {
+  const logout = (close: () => void) => {
     localStorage.removeItem("AUTH_TOKEN");
     queryClient.clear();
+    close();
     navigate("/auth/login");
     queryClient.invalidateQueries({ queryKey: ["user"] });
   };
+
   return (
     <Popover className="relative">
       <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400 mr-10">
-        <Bars3Icon className="w-8 h-8 text-white " />
+        <Bars3Icon className="w-8 h-8 text-white" />
       </Popover.Button>
 
       <Transition
@@ -35,22 +37,32 @@ export default function NavMenu({ name }: NavMenuProps) {
         leaveTo="opacity-0 translate-y-1"
       >
         <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
-          <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
-            <p className="text-center">{name}</p>
-            <Link to="/profile" className="block p-2 hover:text-purple-950">
-              Mi Perfil
-            </Link>
-            <Link to="/" className="block p-2 hover:text-purple-950">
-              Mis Proyectos
-            </Link>
-            <button
-              className="block p-2 hover:text-purple-950"
-              type="button"
-              onClick={logout}
-            >
-              Cerrar Sesión
-            </button>
-          </div>
+          {({ close }) => (
+            <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
+              <p className="text-center">{name}</p>
+              <Link
+                to="/profile"
+                className="block p-2 hover:text-purple-950"
+                onClick={() => close()}
+              >
+                Mi Perfil
+              </Link>
+              <Link
+                to="/"
+                className="block p-2 hover:text-purple-950"
+                onClick={() => close()}
+              >
+                Mis Proyectos
+              </Link>
+              <button
+                className="block p-2 hover:text-purple-950"
+                type="button"
+                onClick={() => logout(close)}
+              >
+                Cerrar Sesión
+              </button>
+            </div>
+          )}
         </Popover.Panel>
       </Transition>
     </Popover>
