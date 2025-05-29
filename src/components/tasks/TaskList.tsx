897591da8755bf1502988e3b7eea4116d3a8a8fa
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-29 21:43:24 +0200

    fix: adjust minimum width of task status columns for better layout

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -88,7 +88,7 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         <DndContext onDragEnd={handleDragEnd}>
           {Object.entries(groupedTasks).map(([status, tasks]) => (
-            <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <div key={status} className="min-w-[275px] 2xl:min-w-0 2xl:w-1/5">
               <h3
                 className={`text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]} text-center`}
               >
